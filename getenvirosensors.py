11dce65a48f8baf8963c2b1132c00347989f5a28
Mark IJzerman <markijzerman@gmail.com>
2017-11-18 17:15:46 +0100

    test foo

--- getenvirosensors.py
+++ getenvirosensors.py
@@ -4,4 +4,6 @@ print("test the code")
 
 def foo(a, b):
     """ foo function does stuff """
-    print("changes yo")
+    print("changes yo. the variables called are {0} and {1}".format(a, b))
+
+foo("Myval1", "Myval2")
